2ac7537a7b7a5d76810e361cac199eefe238907f
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-10-13 13:00:55 +0200

    Renamed constants

--- nanoplayboard/constants.py
+++ nanoplayboard/constants.py
@@ -1,22 +1,23 @@
 
-class Constants:
-    NPB_COMMAND                 = 0x40
-    NPG_BUZZER_PLAY_TONE        = 0x20
-    NPG_BUZZER_STOP_TONE        = 0x21
+class NanoPlayBoardConstants:
+    COMMAND                 = 0x40
 
-    NPB_RGB_ON                  = 0x30
-    NPB_RGB_OFF                 = 0x31
-    NPB_RGB_TOGGLE              = 0x32
-    NPB_RGB_SET_COLOR           = 0x33
-    NPB_RGB_SET_INTENSITY       = 0x34
+    BUZZER_PLAY_TONE        = 0x20
+    BUZZER_STOP_TONE        = 0x21
 
-    NPB_POTENTIOMETER_READ      = 0x40
-    NPB_POTENTIOMETER_SCALE_TO  = 0x41
+    RGB_ON                  = 0x30
+    RGB_OFF                 = 0x31
+    RGB_TOGGLE              = 0x32
+    RGB_SET_COLOR           = 0x33
+    RGB_SET_INTENSITY       = 0x34
 
-    NPB_LDR_READ                = 0x50
-    NPB_LDR_SCALE_TO            = 0x51
+    POTENTIOMETER_READ      = 0x40
+    POTENTIOMETER_SCALE_TO  = 0x41
 
-    NPB_LEDMATRIX_PRINT_CHAR    = 0x60
-    NPB_LEDMATRIX_PRINT_PATTERN = 0x61
-    NPB_LEDMATRIX_PRINT_STRING  = 0x62
-    NPB_LEDMATRIX_PRINT_IN_LAND = 0x63
+    LDR_READ                = 0x50
+    LDR_SCALE_TO            = 0x51
+
+    LEDMATRIX_PRINT_CHAR    = 0x60
+    LEDMATRIX_PRINT_PATTERN = 0x61
+    LEDMATRIX_PRINT_STRING  = 0x62
+    LEDMATRIX_PRINT_IN_LAND = 0x63
