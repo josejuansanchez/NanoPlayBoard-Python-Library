dceadfe030c12acd02c5fcd7723cc1ba43dc4797
José Juan Sánchez <josejuan.sanchez@gmail.com>
2017-03-31 01:25:47 +0200

    Updated constants

--- nanoplayboard/nano_constants.py
+++ nanoplayboard/nano_constants.py
@@ -1,27 +1,48 @@
 
 class NanoConstants:
-    COMMAND                 = 0x10
+    COMMAND                  = 0x10
 
-    BUZZER_PLAY_TONE        = 0x20
-    BUZZER_STOP_TONE        = 0x21
+    BUZZER_PLAY_TONE         = 0x20
+    BUZZER_STOP_TONE         = 0x21
 
-    RGB_ON                  = 0x30
-    RGB_OFF                 = 0x31
-    RGB_TOGGLE              = 0x32
-    RGB_SET_COLOR           = 0x33
-    RGB_SET_INTENSITY       = 0x34
+    RGB_ON                   = 0x22
+    RGB_OFF                  = 0x23
+    RGB_TOGGLE               = 0x24
+    RGB_SET_COLOR            = 0x25
+    RGB_SET_INTENSITY        = 0x26
 
-    POTENTIOMETER_READ      = 0x40
-    POTENTIOMETER_SCALE_TO  = 0x41
+    POTENTIOMETER_READ       = 0x27
+    POTENTIOMETER_SCALE_TO   = 0x28
 
-    LDR_READ                = 0x50
-    LDR_SCALE_TO            = 0x51
+    LDR_READ                 = 0x29
+    LDR_SCALE_TO             = 0x30
 
-    LEDMATRIX_PRINT_CHAR    = 0x60
-    LEDMATRIX_PRINT_PATTERN = 0x61
-    LEDMATRIX_PRINT_STRING  = 0x62
-    LEDMATRIX_PRINT_IN_LAND = 0x63
+    LEDMATRIX_PRINT_CHAR     = 0x31
+    LEDMATRIX_PRINT_PATTERN  = 0x32
+    LEDMATRIX_PRINT_STRING   = 0x33
+    LEDMATRIX_PRINT_IN_LAND  = 0x34
+    LEDMATRIX_STOP_PRINT     = 0x35
 
-    SERVO_TO                = 0x70
+    SERVO_TO                 = 0x36
+    SERVOS_GO_FORWARD        = 0x37
+    SERVOS_GO_BACKWARD       = 0x38
+    SERVOS_GO_RIGHT          = 0x39
+    SERVOS_GO_LEFT           = 0x40
+    SERVOS_SET_SPEED         = 0x41
 
-    ROTARY_ENCODER_READ     = 0x80
+    ROTARY_ENCODER_READ      = 0x42
+
+    ULTRASOUND_READ          = 0x43
+    ULTRASOUND_SCALE_TO      = 0x44
+
+    DHT_READ_TEMPERATURE     = 0x45
+    DHT_READ_HUMIDITY        = 0x46
+
+    BUTTON_TOP_IS_PRESSED    = 0x47
+    BUTTON_BOTTOM_IS_PRESSED = 0x48
+    BUTTON_LEFT_IS_PRESSED   = 0x49
+    BUTTON_RIGHT_IS_PRESSED  = 0x50
+
+    ACCELEROMETER_GET_X      = 0x51
+    ACCELEROMETER_GET_Y      = 0x52
+    ACCELEROMETER_GET_Z      = 0x53
