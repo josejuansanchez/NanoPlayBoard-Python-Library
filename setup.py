87c2b95fe9ac598332174e4cfa2212323f5bf183
José Juan Sánchez <josejuan.sanchez@gmail.com>
2017-04-15 12:34:32 +0200

    Added dependencies and minor issues in setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 setup(name='nanoplayboard',
       version='0.0.1',
       description='A Python Library for NanoPlayBoard Firmata',
-      keywords='nanoplayboard arduino python firmata',
+      keywords=['NanoPlayBoard', 'Arduino', 'Firmata'],
       url='https://github.com/josejuansanchez/NanoPlayBoard-Python-Library',
       author='Jose Juan Sanchez',
       author_email='josejuan.sanchez@gmail.com',
-      license='GPL-3.0',
+      license='GNU General Public License v3 (GPLv3)',
+      install_requires=['pymata_aio'],
       packages=['nanoplayboard'],
       zip_safe=False)
