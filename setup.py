66eaee9f15486cd46698c60421ab8de116ad987e
José Juan Sánchez <josejuan.sanchez@gmail.com>
2017-04-15 12:07:13 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
 
 setup(name='nanoplayboard',
-      version='0.1.0',
+      version='0.0.1',
       description='A Python Library for NanoPlayBoard Firmata',
+      keywords='nanoplayboard arduino python firmata',
       url='https://github.com/josejuansanchez/NanoPlayBoard-Python-Library',
       author='José Juan Sánchez',
       author_email='josejuan.sanchez@gmail.com',
